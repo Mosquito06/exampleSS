1fc4239f5c48511fc17ad48cc363d1f0013affce
chinchin <skykim10908@naver.com>
2022-10-23 21:42:06 +0900

    spring security form logi

--- src/main/java/com/example/ss/SecurityController.java
+++ src/main/java/com/example/ss/SecurityController.java
@@ -11,4 +11,12 @@ public class SecurityController
     {
         return "home";
     }
+
+    @GetMapping("/loginPage")
+    public String loginPage()
+    {
+        return "loginPage";
+    }
+
+
 }
